a7afd4a5ccdfcf5cabfba717ef8991cbe803ff03
Charles <charles@reptile.ca>
2012-12-25 12:24:18 -0800

    header file

---
+++ src/numphp.h
@@ -0,0 +1,36 @@
+/*
+	Author: Charles Iliya Krempeaux  <charles@reptile.ca>
+*/
+
+
+#ifndef PHP_NUMPHP_H
+#define PHP_NUMPHP_H 1
+
+#define PHP_NUMPHP_VERSION "1.0"
+#define PHP_NUMPHP_EXTNAME "numphp"
+
+#include "php.h"
+
+
+
+
+
+PHP_FUNCTION(num_test);
+
+PHP_FUNCTION(num_vec2);
+PHP_FUNCTION(num_ivec2);
+
+PHP_FUNCTION(num_vec3);
+PHP_FUNCTION(num_ivec3);
+
+PHP_FUNCTION(num_vec4);
+PHP_FUNCTION(num_ivec4);
+
+
+
+
+
+extern zend_module_entry numphp_module_entry;
+#define phpext_numphp_ptr &numphp_module_entry
+
+#endif /* PHP_NUMPHP_H */
