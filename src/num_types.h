730fcfe62ce1cf524238f1ec0a228902eacb6cbc
Charles <charles@reptile.ca>
2012-12-26 00:36:04 -0800

    dev work

--- src/num_types.h
+++ src/num_types.h
@@ -3,20 +3,20 @@
 
 
 
-static int le_num_ivec2;
+extern int le_num_ivec2;
 typedef struct _php_num_ivec2 {
     long x;
     long y;
 } php_num_ivec2;
 
-static int le_num_ivec3;
+extern int le_num_ivec3;
 typedef struct _php_num_ivec3 {
     long x;
     long y;
     long z;
 } php_num_ivec3;
 
-static int le_num_ivec4;
+extern int le_num_ivec4;
 typedef struct _php_num_ivec4 {
     long x;
     long y;
@@ -26,20 +26,20 @@ typedef struct _php_num_ivec4 {
 
 
 
-static int le_num_vec2;
+extern int le_num_vec2;
 typedef struct _php_num_vec2 {
     double x;
     double y;
 } php_num_vec2;
 
-static int le_num_vec3;
+extern int le_num_vec3;
 typedef struct _php_num_vec3 {
     double x;
     double y;
     double z;
 } php_num_vec3;
 
-static int le_num_vec4;
+extern int le_num_vec4;
 typedef struct _php_num_vec4 {
     double x;
     double y;
