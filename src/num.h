43b5110eecf3bc8184cd59ec8985264328d86c29
Charles <charles@reptile.ca>
2012-12-25 22:38:03 -0800

    dev work

--- src/num.h
+++ src/num.h
@@ -20,48 +20,50 @@ PHP_FUNCTION(num_test);
 PHP_FUNCTION(num_mathml_serialize);
 PHP_FUNCTION(num_mathml_unserialize);
 
-PHP_FUNCTION(num_vec2);
+//PHP_FUNCTION(num_array_serialize);
+//PHP_FUNCTION(num_array_unserialize);
+
 PHP_FUNCTION(num_ivec2);
+PHP_FUNCTION(num_vec2);
 
-PHP_FUNCTION(num_vec3);
 PHP_FUNCTION(num_ivec3);
+PHP_FUNCTION(num_vec3);
 
-PHP_FUNCTION(num_vec4);
 PHP_FUNCTION(num_ivec4);
+PHP_FUNCTION(num_vec4);
 
 
-//PHP_FUNCTION(num_mat2x2);
 //PHP_FUNCTION(num_imat2x2);
+//PHP_FUNCTION(num_mat2x2);
 //
-//PHP_FUNCTION(num_mat2x3);
 //PHP_FUNCTION(num_imat2x3);
+//PHP_FUNCTION(num_mat2x3);
 //
-//PHP_FUNCTION(num_mat2x4);
 //PHP_FUNCTION(num_imat2x4);
+//PHP_FUNCTION(num_mat2x4);
 //
 //
-//PHP_FUNCTION(num_mat3x2);
 //PHP_FUNCTION(num_imat3x2);
+//PHP_FUNCTION(num_mat3x2);
 //
-//PHP_FUNCTION(num_mat3x3);
 //PHP_FUNCTION(num_imat3x3);
+//PHP_FUNCTION(num_mat3x3);
 //
-//PHP_FUNCTION(num_mat3x4);
 //PHP_FUNCTION(num_imat3x4);
+//PHP_FUNCTION(num_mat3x4);
 //
 //
-//PHP_FUNCTION(num_mat4x2);
 //PHP_FUNCTION(num_imat4x2);
+//PHP_FUNCTION(num_mat4x2);
 //
-//PHP_FUNCTION(num_mat4x3);
 //PHP_FUNCTION(num_imat4x3);
+//PHP_FUNCTION(num_mat4x3);
 //
-//PHP_FUNCTION(num_mat4x4);
 //PHP_FUNCTION(num_imat4x4);
-//
-//
-//PHP_FUNCTION(num_array_out);
-//PHP_FUNCTION(num_array_in);
+//PHP_FUNCTION(num_mat4x4);
+
+
+PHP_FUNCTION(num_add);
 
 
 
