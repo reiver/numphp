236c8cd1f6dc3a9d19e60c3b7446a39f6b038e7e
Charles <charles@reptile.ca>
2012-12-25 21:55:18 -0800

    dev work

--- src/num.c
+++ src/num.c
@@ -357,6 +357,7 @@ PHP_FUNCTION(num_mathml_serialize)
 				//snprintf(x_mem, x_mem_len, "%ld", ivec3->x);
 				//snprintf(y_mem, y_mem_len, "%ld", ivec3->y);
 				//snprintf(z_mem, z_mem_len, "%ld", ivec3->z);
+				//snprintf(w_mem, w_mem_len, "%ld", ivec3->w);
 				sprintf(x_mem, "%ld", ivec4->x);
 				sprintf(y_mem, "%ld", ivec4->y);
 				sprintf(z_mem, "%ld", ivec4->z);
@@ -522,6 +523,83 @@ PHP_FUNCTION(num_mathml_serialize)
 			} else if (  resource_type_id == le_num_vec4  ) {
 
 				php_num_vec4 *vec4;
+
+				ZEND_FETCH_RESOURCE(vec4, php_num_vec4 *, &user_variable, -1, "num_vec4", le_num_vec4);
+
+
+
+				char *prefix  = "<vector><cn>";
+				int   prefix_len;
+
+				char *middle  = "</cn><cn>";
+				int   middle_len;
+
+				char *postfix = "</cn></vector>";
+				int   postfix_len;
+
+				char *x_mem;
+				int   x_mem_len;
+
+				char *y_mem;
+				int   y_mem_len;
+
+				char *z_mem;
+				int   z_mem_len;
+
+				char *w_mem;
+				int   w_mem_len;
+
+				char *mem;
+				int   mem_len;
+
+
+				prefix_len  = strlen(prefix);
+				middle_len  = strlen(middle);
+				postfix_len = strlen(postfix);
+
+				x_mem_len = snprintf(NULL, 0, "%f", vec4->x);
+				y_mem_len = snprintf(NULL, 0, "%f", vec4->y);
+				z_mem_len = snprintf(NULL, 0, "%f", vec4->z);
+				w_mem_len = snprintf(NULL, 0, "%f", vec4->w);
+
+				x_mem = emalloc(x_mem_len+1);
+				y_mem = emalloc(y_mem_len+1);
+				z_mem = emalloc(z_mem_len+1);
+				w_mem = emalloc(w_mem_len+1);
+
+				//snprintf(x_mem, x_mem_len, "%f", vec4->x);
+				//snprintf(y_mem, y_mem_len, "%f", vec4->y);
+				//snprintf(z_mem, z_mem_len, "%f", vec4->z);
+				//snprintf(w_mem, w_mem_len, "%f", vec4->w);
+				sprintf(x_mem, "%f", vec4->x);
+				sprintf(y_mem, "%f", vec4->y);
+				sprintf(z_mem, "%f", vec4->z);
+				sprintf(w_mem, "%f", vec4->w);
+
+
+				mem_len = prefix_len + x_mem_len + middle_len + y_mem_len + middle_len + z_mem_len + middle_len + w_mem_len + postfix_len;
+
+				mem = emalloc(mem_len);
+
+				memcpy( mem                                                                                                     , prefix  , prefix_len  );
+				memcpy( mem + prefix_len                                                                                        , x_mem   , x_mem_len   );
+				memcpy( mem + prefix_len + x_mem_len                                                                            , middle  , middle_len  );
+				memcpy( mem + prefix_len + x_mem_len + middle_len                                                               , y_mem   , y_mem_len   );
+				memcpy( mem + prefix_len + x_mem_len + middle_len + y_mem_len                                                   , middle  , middle_len  );
+				memcpy( mem + prefix_len + x_mem_len + middle_len + y_mem_len + middle_len                                      , z_mem   , z_mem_len   );
+				memcpy( mem + prefix_len + x_mem_len + middle_len + y_mem_len + middle_len + z_mem_len                          , middle  , middle_len  );
+				memcpy( mem + prefix_len + x_mem_len + middle_len + y_mem_len + middle_len + z_mem_len + middle_len             , w_mem   , w_mem_len  );
+				memcpy( mem + prefix_len + x_mem_len + middle_len + y_mem_len + middle_len + z_mem_len + middle_len + w_mem_len , postfix , postfix_len );
+
+
+				efree(x_mem);
+				efree(y_mem);
+				efree(z_mem);
+				efree(w_mem);
+
+
+/////////////// RETURN
+				RETURN_STRINGL(mem, mem_len, 0);
 
 			} else {
 
