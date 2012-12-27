f59ff170a81d6fd861b3d28c926881c28464542b
Charles <charles@reptile.ca>
2012-12-26 20:37:00 -0800

    dev work

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ cp src/config.m4        BUILD/
 cp src/config.w32       BUILD/
 cp src/num.h            BUILD/
 cp src/num.c            BUILD/
-cp src/num_types.h      BUILD/
 cp src/num_add.c        BUILD/
 cp src/num_dimension.c  BUILD/
+cp src/num_dimension.h  BUILD/
 cp src/num_mathml.c     BUILD/
 cp src/num_types.c      BUILD/
+cp src/num_types.h      BUILD/
 cd BUILD
 phpize
 ./configure --enable-num
